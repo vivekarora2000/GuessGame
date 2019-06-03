6c763f09aec318ec95c33ed397e0062beee8324e
Vivek Arora <f20181016@goa.bits-pilani.ac.in>
2019-06-03 18:43:21 +0530

    Version 1.1

--- GuessGame.py
+++ GuessGame.py
@@ -1,7 +1,13 @@
 from random import randint
-a = randint(1,10)
+a = randint(1,100)
+print("WELCOME TO GUESS ME!")
+print("I'm thinking of a number between 1 and 100")
+print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
+print("If your guess is within 10 of my number, I'll tell you you're WARM")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
 guess=[]
-i=0;
 while True:
     g=int(input('Enter Guess:'))
     guess.append(g)
